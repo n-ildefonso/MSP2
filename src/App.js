321525ce04aac9161323437e4028ced5e1be3772
Jose Torres <jrtp22@gmail.com>
2023-09-30 09:58:54 -0700

    added a api to the homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+    let [search, setSearch] = useState('')
+    let [message, setMessage] = useState('Search for Pokemon!')
+    let [data, setData] = useState([])
+
+    useEffect(() => {
+      const fetchData = async () => {
+          document.title = `${search} Pokemon`
+          const response = await fetch('https://pokeapi.co/api/v2/pokemon/search?term=')
+          const resData = await response.json()
+          if (resData.results.length > 0) {
+              setData(resData.results)
+          } else {
+              setMessage('Not Found')
+          }
+      }
+      fetchData()
+  }, [search])
+  
+    return (
+        <div>
+
+        </div>
+    )
 }
 
-export default App;
+export default App
+
