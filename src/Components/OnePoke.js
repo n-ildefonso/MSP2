6e8e9f22d454ff8aaaadbe8fea3cc35c12b51d85
n-ildefonso <nessfonso@gmail.com>
2023-10-09 10:47:55 -0700

    axios

--- src/Components/OnePoke.js
+++ src/Components/OnePoke.js
@@ -1,3 +1,36 @@
-import 'bootstrap/dist/css/bootsrap.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-fetch('https://pokeapi.co/api/v2/pokemon/search?term=');
+const Pokemon = ({ term }) => {
+  const [pokemonData, setPokemonData] = useState(null);
+
+  useEffect(() => {
+    // Make an API request to fetch the Pokémon data
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${term}`)
+      .then((response) => {
+        setPokemonData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching Pokémon data:', error);
+      });
+  }, [term]);
+
+  if (!pokemonData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h2>{pokemonData.name}</h2>
+      <img
+        src={pokemonData.sprites.front_default}
+        alt={`${pokemonData.name} sprite`}
+      />
+      <p>Height: {pokemonData.height} decimetres</p>
+      <p>Weight: {pokemonData.weight} hectograms</p>
+    </div>
+  );
+};
+
+export default Pokemon;
